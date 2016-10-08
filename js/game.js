054ef0abb3b17b6ff76892e5687f9872de55dbc6
Ralph Maamari <ralphpal@hotmail.com>
2016-10-08 09:54:27 -0400

    Added files

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+var socket // Socket connection
+
 $(document).ready(function(){
 	console.log("connected");
 	//Canvas stuff
@@ -5,16 +7,124 @@ $(document).ready(function(){
 	var ctx = canvas.getContext("2d");
 	var w = $("#canvas").width();
 	var h = $("#canvas").height();
-	
+
 	//Lets save the cell width in a variable for easy control
 	var cw = 10;
 	var d;
 	var food;
 	var score;
-	
+
 	//Lets create the snake now
 	var snake_array; //an array of cells to make up the snake
-	
+
+	// start of insert
+function create(){
+    socket = io.connect()
+
+    setEventHandlers()
+}
+
+var setEventHandlers = function () {
+  // Socket connection successful
+  socket.on('connect', onSocketConnected)
+
+  // Socket disconnection
+  socket.on('disconnect', onSocketDisconnect)
+
+  // New player message received
+  socket.on('new player', onNewPlayer)
+
+  // Player move message received
+  socket.on('move player', onMovePlayer)
+
+  // Player removed message received
+  socket.on('remove player', onRemovePlayer)
+}
+
+// Socket connected
+function onSocketConnected () {
+  console.log('Connected to socket server')
+
+  // Send local player data to the game server
+  socket.emit('new player', { x: player.x, y: player.y, angle: player.angle })
+}
+
+// Socket disconnected
+function onSocketDisconnect () {
+  console.log('Disconnected from socket server')
+}
+
+// New player
+function onNewPlayer (data) {
+  console.log('New player connected:', data.id)
+
+  // Avoid possible duplicate players
+  var duplicate = playerById(data.id)
+  if (duplicate) {
+    console.log('Duplicate player!')
+    return
+  }
+
+  // Add new player to the remote players array
+  enemies.push(new RemotePlayer(data.id, game, player, data.x, data.y, data.angle))
+}
+
+// Move player
+function onMovePlayer (data) {
+  var movePlayer = playerById(data.id)
+
+  // Player not found
+  if (!movePlayer) {
+    console.log('Player not found: ', data.id)
+    return
+  }
+
+  // Update player position
+  movePlayer.player.x = data.x
+  movePlayer.player.y = data.y
+  movePlayer.player.angle = data.angle
+}
+
+// Remove player
+function onRemovePlayer (data) {
+  var removePlayer = playerById(data.id)
+
+  // Player not found
+  if (!removePlayer) {
+    console.log('Player not found: ', data.id)
+    return
+  }
+
+  removePlayer.player.kill()
+
+  // Remove player from array
+  enemies.splice(enemies.indexOf(removePlayer), 1)
+}
+
+function update () {
+  for (var i = 0; i < enemies.length; i++) {
+    if (enemies[i].alive) {
+      enemies[i].update()
+      game.physics.arcade.collide(player, enemies[i].player)
+    }
+  }
+
+  if (cursors.left.isDown) {
+    player.angle -= 4
+  } else if (cursors.right.isDown) {
+    player.angle += 4
+  }
+
+  if (cursors.up.isDown) {
+    // The speed we'll travel at
+    currentSpeed = 300
+  } else {
+    if (currentSpeed > 0) {
+      currentSpeed -= 4
+    }
+  } // end of insert
+
+
 	function init()
 	{
 		d = "right"; //default direction
@@ -22,14 +132,14 @@ $(document).ready(function(){
 		create_food(); //Now we can see the food particle
 		//finally lets display the score
 		score = 0;
-		
+
 		//Lets move the snake now using a timer which will trigger the paint function
 		//every 60ms
 		if(typeof game_loop != "undefined") clearInterval(game_loop);
 		game_loop = setInterval(paint, 60);
 	}
 	init();
-	
+
 	function create_snake()
 	{
 		var length = 5; //Length of the snake
@@ -40,18 +150,18 @@ $(document).ready(function(){
 			snake_array.push({x: i, y:0});
 		}
 	}
-	
+
 	//Lets create the food now
 	function create_food()
 	{
 		food = {
-			x: Math.round(Math.random()*(w-cw)/cw), 
-			y: Math.round(Math.random()*(h-cw)/cw), 
+			x: Math.round(Math.random()*(w-cw)/cw),
+			y: Math.round(Math.random()*(h-cw)/cw),
 		};
 		//This will create a cell with x/y between 0-44
 		//Because there are 45(450/10) positions accross the rows and columns
 	}
-	
+
 	//Lets paint the snake now
 	function paint()
 	{
@@ -61,7 +171,7 @@ $(document).ready(function(){
 		ctx.fillRect(0, 0, w, h);
 		ctx.strokeStyle = "black";
 		ctx.strokeRect(0, 0, w, h);
-		
+
 		//The movement code for the snake to come here.
 		//The logic is simple
 		//Pop out the tail cell and place it infront of the head cell
@@ -74,7 +184,7 @@ $(document).ready(function(){
 		else if(d == "left") nx--;
 		else if(d == "up") ny--;
 		else if(d == "down") ny++;
-		
+
 		//Lets add the game over clauses now
 		//This will restart the game if the snake hits the wall
 		//Lets add the code for body collision
@@ -86,7 +196,7 @@ $(document).ready(function(){
 			//Lets organize the code a bit now.
 			return;
 		}
-		
+
 		//Lets write the code to make the snake eat the food
 		//The logic is simple
 		//If the new head position matches with that of the food,
@@ -104,23 +214,23 @@ $(document).ready(function(){
 			tail.x = nx; tail.y = ny;
 		}
 		//The snake can now eat the food.
-		
+
 		snake_array.unshift(tail); //puts back the tail as the first cell
-		
+
 		for(var i = 0; i < snake_array.length; i++)
 		{
 			var c = snake_array[i];
 			//Lets paint 10px wide cells
 			paint_cell(c.x, c.y);
 		}
-		
+
 		//Lets paint the food
 		paint_cell(food.x, food.y);
 		//Lets paint the score
 		var score_text = "Score: " + score;
 		ctx.fillText(score_text, 5, h-5);
 	}
-	
+
 	//Lets first create a generic function to paint cells
 	function paint_cell(x, y)
 	{
@@ -129,7 +239,7 @@ $(document).ready(function(){
 		ctx.strokeStyle = "white";
 		ctx.strokeRect(x*cw, y*cw, cw, cw);
 	}
-	
+
 	function check_collision(x, y, array)
 	{
 		//This function will check if the provided x/y coordinates exist
@@ -141,7 +251,7 @@ $(document).ready(function(){
 		}
 		return false;
 	}
-	
+
 	//Lets add the keyboard controls now
 	$(document).keydown(function(e){
 		var key = e.which;
@@ -152,11 +262,11 @@ $(document).ready(function(){
 		else if(key == "40" && d != "up") d = "down";
 		//The snake is now keyboard controllable
 	})
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 });

84be3ffac73ad08f0f4f0f98c9594475953320e1
Yuxiang Dai <yuxiang.dai43@gmail.com>
2016-10-09 00:19:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+// server.listen(app.get('port'))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+io.on('connection', function(socket){
+  console.log('hello');
+
+  socket.on('score', function(data) {
+    console.log(data)
+    socket.broadcast.emit('score', data);
+  })
+
+  socket.on('snake', function(arr) {
+    socket.broadcast.emit('snake', arr);
+  })
+});
+
+
+
+
+// // error handlers
+
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+
+server.listen(80, function(){
+  console.log('listening on *:80');
+});
+
+module.exports = app;
